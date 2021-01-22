2afec2223365f50238866d2f1167e2c48b8f8c66
Liana <49907045+Liana-Yuri@users.noreply.github.com>
2021-01-22 20:51:52 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,78 @@
+
+
+
+
+var  acc = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");    
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });  
+}
+
+
+
+function calcular(){
+  let edad,genero;precio; 
+ 
+		precio = 100;
+		//Miramos el genero
+		genero = document.formulario.genero.value;
+		if(genero == "hombre"){
+			precio = precio + 25;
+		}
+ //Miramos la edad
+ edad = document.formulario.edad.value;
+ edad = parseInt(edad);
+ if(edad < 25){
+   precio = precio - 20;
+ }
+  }
+  //Consultamos si ha elegido dosnacionalidad		
+  if(document.formulario.dosnacionalidad.checked == true){
+    
+    precio = precio + 40;
+  }	
+  //Consultamos si ha elegido spa		
+  if(document.formulario.coche.checked == true){  
+    precio = precio + 60;
+  }	
+  //Consultamos si ha elegido zumba		
+  if(document.formulario.zumba.checked == true){
+    precio = precio + 30;
+  }	
+  document.formulario.precio.value = precio + "€";	
+}
+function enviar(){
+  var repuesta,precio;
+  precio = document.formulario.precio.value;
+  respuesta = confirm("Su precio es " + precio + "\n¿Desea continuar?");
+  if(respuesta == true){
+    document.formulario.submit();
+  }
+}
+
+
+
+
+
+ 
+
+
+  
+    
+  
+     
+        
+  
